ea4084710b1a2b0a6bdff2dd351ae97713f33522
Glacial <glacial@gmail.com>
2019-06-19 01:42:54 -0400

    Add initial file structure

---
+++ README.md
@@ -0,0 +1,45 @@
+# Trajes Fatais (2019)
+Front-end Development of the brazilian game's website.
+
+## Features
+- <a href="https://github.com/thedaviddias/Front-End-Checklist">Front-end checklist</a> ready
+- <a href="https://sass-lang.com/">SASS</a> ready
+- <a href="https://handlebarsjs.com/">Handlebars</a> templates
+- Minified HTML
+- Minifiend JS
+- Optmized Images
+
+
+### Live Demo: 
+https://marceloglacial.github.io/2019-trajes-fatais/app/dist/
+
+
+## Installation
+
+- [Install npm](https://www.npmjs.com/get-npm)
+- [Install Gulp](https://gulpjs.com)
+
+Run install on terminal: 
+
+```terminal
+npm install 
+```
+
+# How to Use
+
+- `npm start` - Start front-end developement
+- `npm run build` - Build project (minifed HTML, minified CSS, minified JS and optmized images).
+
+# Design 
+
+### Prototypes
+
+- <a href="https://xd.adobe.com/view/eb03a5a8-e414-47ec-48ce-e9da2e800acf-9d32/screen/2d8e3b3f-8f6f-40e2-90d9-e9d603a52666/Trajes-fatais-V3-1/?fullscreen">Desktop</a>
+
+- <a href="https://xd.adobe.com/view/eb03a5a8-e414-47ec-48ce-e9da2e800acf-9d32/screen/2d8e3b3f-8f6f-40e2-90d9-e9d603a52666/Trajes-fatais-V3-1/?fullscreen">Mobile</a>
+
+
+### Design Specs / Handoff
+
+- <a href="https://xd.adobe.com/spec/30b63a57-ffbf-4cec-55f8-2f2ea0d8e9e3-4687/">Desktop</a>
+- <a href="https://xd.adobe.com/spec/58896dae-06f4-45c4-7cd0-2ac3c6817191-2eb8/">Mobile</a>
---
+++ gulpfile.js
@@ -0,0 +1,225 @@
+// =============================================================
+// 1. Configuration
+// =============================================================
+
+// 1.1 - Gulp Packages
+// ------------------------------
+const gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    handlebars = require('gulp-compile-handlebars'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
+    purgecss = require('gulp-purgecss'),
+    concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin');
+
+
+// ==============================================================
+// 2. Functions
+// ==============================================================
+
+// 2.1 - Clean folder
+// ------------------------------
+function clean(path) {
+    return del(path);
+};
+
+// 2.2 - Complie SASS
+// ------------------------------
+function styles(src, dest, minify) {
+    let options;
+    if (minify) {
+        options = {
+            outputStyle: 'compressed'
+        }
+    }
+    return gulp
+        .src(src + '**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass(options))
+        .on('error', sass.logError)
+        .pipe(autoprefixer())
+        // .pipe(sourcemaps.write())
+        .pipe(concat('main.min.css'))
+        .pipe(gulp.dest(dest))
+};
+
+function purge(dest) {
+    return gulp.src(dest + '**/*.css')
+        .pipe(purgecss({
+            content: [dest + '**/*.html']
+        }))
+        .pipe(gulp.dest(dest))
+};
+
+// 2.3 - Minimize Scripts
+// ------------------------------
+function scripts(src, dist) {
+    return (
+        gulp
+        .src(src + '**/*.js', {
+            sourcemaps: true
+        })
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest(dist))
+    );
+};
+
+// 2.4 - Optmize images
+// ------------------------------
+function images(src, dest) {
+    return (
+        gulp
+        .src(src + folders)
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 5,
+            svgoPlugins: [
+                {
+                    removeViewBox: true
+                }
+            ]
+        }))
+        .pipe(gulp.dest(dest))
+    )
+};
+
+// 2.5 - Complie templates
+// ------------------------------
+function templates(templates, partials, dest) {
+    var templateData = {},
+        options = {
+            ignorePartials: true,
+            batch: [partials]
+        }
+    return gulp.src(templates + '*.html')
+        .pipe(handlebars(templateData, options))
+        .pipe(gulp.dest(dest))
+};
+
+// 2.6 - Copy
+// ------------------------------
+function copy(src, dest) {
+    return gulp.src(src)
+        .pipe(gulp.dest(dest));
+};
+
+// 2.7 - Start server
+// ------------------------------
+function liveServer(path) {
+    let options = {
+        server: {
+            baseDir: frontend.dist
+        }
+    }
+    gulp.watch(frontend.src).on('change', gulp.series('frontend:develop', liveReload));
+
+    browserSync.init(options);
+};
+
+// 2.8 - Reload page
+// ------------------------------
+function liveReload() {
+    browserSync.reload();
+};
+
+// 2.9 - Minify Html 
+// ------------------------------
+function html(src, dest) {
+    return gulp.src([src + '**/*.html'])
+      .pipe(htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+      }))
+      .pipe(gulp.dest(dest));
+  };
+
+// =============================================================
+// 3. Front-end
+// =============================================================
+
+// 3.1 - Paths
+// ------------------------------
+const all = '**/*.*',
+    folders = '**/*';
+
+const frontend = new function () {
+    this.root = 'app/';
+    this.all = this.root + all;
+    this.src = this.root + 'src/';
+    this.dist = this.root + 'dist/';
+    this.assets = this.src + 'assets/' + folders;
+    this.components = this.src + 'components/';
+};
+
+// 3.2 - Templates
+// ------------------------------
+gulp.task('frontend:templates', () => templates(frontend.src, frontend.components, frontend.dist));
+
+// 3.3 - Assets
+// ------------------------------
+gulp.task('frontend:assets', () => copy(frontend.assets, frontend.dist));
+
+// 3.4 - Styles
+// ------------------------------
+gulp.task('frontend:styles', () => styles(frontend.components, frontend.dist));
+gulp.task('frontend:styles-build', () => styles(frontend.components, frontend.dist, 'minify'));
+gulp.task('frontend:purgecss', () => purge(frontend.dist));
+
+// 3.5 - Scripts
+// ------------------------------
+gulp.task('frontend:scripts', () => scripts(frontend.components, frontend.dist));
+
+// 3.6 - Images
+// ------------------------------
+gulp.task('frontend:images', () => images(frontend.dist, frontend.dist));
+
+// 3.7 - HTML
+// ------------------------------
+gulp.task('frontend:html', () => html(frontend.dist, frontend.dist));
+
+// 3.8 - Clean build files
+// ------------------------------
+gulp.task('frontend:clean', () => clean(frontend.dist));
+
+// 3.9 - Develop
+// ------------------------------
+gulp.task('frontend:develop',
+    gulp.series(
+        'frontend:clean',
+        'frontend:assets',
+        'frontend:styles',
+        'frontend:scripts',
+        'frontend:templates',
+        'frontend:purgecss',
+    )
+);
+
+// 3.10 - Build
+// ------------------------------
+gulp.task('frontend:build',
+    gulp.series(
+        'frontend:clean',
+        'frontend:assets',
+        'frontend:styles-build',
+        'frontend:scripts',
+        'frontend:templates',
+        'frontend:purgecss',
+        'frontend:html',
+        'frontend:images'
+    )
+);
+
+// 3.11 - Start Server
+// ------------------------------
+gulp.task('frontend:start', gulp.series('frontend:develop', () => liveServer('frontend')));
