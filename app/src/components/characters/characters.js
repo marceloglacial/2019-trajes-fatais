1e89014e5879cfd6f37810ab7b99c11ebd773916
Glacial <glacial@gmail.com>
2019-07-10 02:56:36 -0400

    Add modal first styles

--- app/src/components/characters/characters.js
+++ app/src/components/characters/characters.js
@@ -17,13 +17,17 @@ let prev = $('.characters__list-controls__item--prev');
 let next = $('.characters__list-controls__item--next');
 let all = $('.characters__list-controls__item--all');
 let modal = $('.characters__list-modal');
+let modalClose = $('.characters__list-modal__close');
 let currentSlide = 1;
 let maxScroll = Math.ceil(items / itemsVisible)
 let slideOffset = itemWidth * itemsVisible;
 
 // Initi
-$(document).ready(function(){
-    container.css({'transform': 'translate(' + containerInitialPosition + 'px)'});
+$(document).ready(function () {
+    container.css({
+        'transform': 'translate(' + containerInitialPosition + 'px)',
+        'opacity': 1
+    });
 });
 
 // Remove link
@@ -32,28 +36,55 @@ btn.on('click', function (e) {
 });
 
 // Scroll left
-prev.on('click', function (e) {
+function scrollLefet() {
     container.css({
         'transform': 'translate(' + containerInitialPosition + 'px)'
     });
     currentSlide = 1;
+
+}
+prev.on('click', function (e) {
+    scrollLefet();
 });
 
 // Scroll right
-next.on('click', function (e) {
-    if(currentSlide < maxScroll) {
+function scrollRight() {
+    if (currentSlide < maxScroll) {
         container.css({
             'transform': 'translate(-' + ((currentSlide * slideOffset) - containerInitialPosition) + 'px)'
         });
         currentSlide++;
     }
+}
+next.on('click', function (e) {
+    scrollRight();
 });
+
+// Detect swipe
+var touchInit = 0;
+var touchLast = 0;
+
+document.addEventListener('touchstart', function (e) {
+    touchInit = e.changedTouches[0].pageX;
+}, false);
+
+document.addEventListener('touchend', function (e) {
+    touchLast = e.changedTouches[0].pageX;
+    if ((touchLast - touchInit) < 0) {
+        scrollRight();
+    } else {
+        scrollLefet();
+    }
+}, false);
+
 
 // Display all characters
 all.on('click', function (e) {
     controls.fadeOut()
     parent.fadeOut(200);
-    container.css({'transform': 'translate(0px)'});
+    container.css({
+        'transform': 'translate(0px)'
+    });
     setTimeout(() => {
         container.addClass('characters__list-items--all')
         container.addClass('container')
@@ -65,6 +96,12 @@ all.on('click', function (e) {
 item.on('click', function (e) {
     e.preventDefault();
     modal.addClass('characters__list-modal--open');
+})
+
+// Close modal
+modalClose.on('click', function (e) {
+    e.preventDefault();
+    modal.removeClass('characters__list-modal--open');
 })
 
 // Close modal on ESC
